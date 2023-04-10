1f8b8bc0352c572f453b2dadec52597c3432062d
yem <yem@code.com>
2023-04-10 23:19:45 +0800

    抽象分层优于标签类

---
+++ src/main/java/com/code/yem/effectiveJava/EffectiveConstans.java
@@ -0,0 +1,15 @@
+package com.code.yem.effectiveJava;
+
+/**
+ * @Author ym
+ * @Date 2023/4/10 22:45
+ * @Description:yem
+ */
+public class EffectiveConstans {
+    private EffectiveConstans() {
+    }
+
+    ;
+
+    public static final String CODE = "TEST";
+}
